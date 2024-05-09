f4ddcda3403ef263ee677774560783430ea0cc08
Marco Paggetti <marco.paggetti@studio.unibo.it>
2024-05-09 19:36:31 +0200

    added Handler

--- Elaborato_Finale/Socket_Client_Chat.py
+++ Elaborato_Finale/Socket_Client_Chat.py
@@ -38,6 +38,9 @@ def receive(client):
                 message = client.recv(1024).decode("utf-8")
                 message_list.insert(tk.END, message + '\n')
                 print(message)
+                if message == "quit":
+                    clientSocket.close()
+                    window.quit()
 
             # Controllo degli errori
             except OSError:
